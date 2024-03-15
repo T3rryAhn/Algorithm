ee1423a56e023f8e68f3b81ceb6dbdbb75ef2e49
T3rry <t3rryahn.dev@gmail.com>
2024-03-15 19:54:36 +0900

    [Bronze II] Title: 숫자의 개수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -1,26 +1,13 @@
-numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-num_count_results = [0] * 10
-
-
-def check_number(number: int) -> int:
-    for i in numbers:
-        if number == i:
-            return numbers.index(i)
-    return -1
-
-
-def count_numbers(number: str):
-    for i in range(len(number)):
-        index = check_number(int(number[i]))
-        num_count_results[index] += 1
-
-
 a = int(input())
 b = int(input())
 c = int(input())
+
 multi = a * b * c
 
-count_numbers(str(multi))
+list = [0] * 10
 
-for i in num_count_results:
+for c in str(multi):
+    list[int(c)] += 1
+
+for i in list:
     print(i)

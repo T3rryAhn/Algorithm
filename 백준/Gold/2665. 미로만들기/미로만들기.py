852d34dacb0ebbfd473f4c0262ffd07aca82f978
T3rry <t3rryahn.dev@gmail.com>
2024-03-27 00:30:42 +0900

    [Gold IV] Title: 미로만들기, Time: 124 ms, Memory: 34052 KB -BaekjoonHub

--- 백준/Gold/2665. 미로만들기/미로만들기.py
+++ 백준/Gold/2665. 미로만들기/미로만들기.py
@@ -2,40 +2,36 @@ import sys
 from collections import deque
 
 n = int(sys.stdin.readline())
-grid = [list(map(int, sys.stdin.readline().strip())) for _ in range(n)]
+maze = [list(map(int, sys.stdin.readline().strip())) for _ in range(n)]
 INF = sys.maxsize
-
-destroyed = [
-    [sys.maxsize] * n
-    for _ in range(n)
-]
-
-dx = [-1, 0, 1, 0]
-dy = [0, -1, 0, 1]
-
+destroyed = [[sys.maxsize] * n for _ in range(n)]
+dx = (-1, 0, 1, 0)
+dy = (0, -1, 0, 1)
 
 def in_range(x, y):
-    return 0 <= x and x < n and 0 <= y and y < n
+    return 0 <= x < n and 0 <= y < n
 
-
-def bfs(x, y, c):
+def bfs():
     queue = deque()
-    queue.append((x, y, c))
-    destroyed[x][y] = 0
+    queue.append((0, 0, 0))
+    destroyed[0][0] = 0
+
     while queue:
-        x, y, c = queue.popleft()
+        x, y, d = queue.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
+            temp_d = d
 
             if in_range(nx, ny):
-                if grid[nx][ny] and destroyed[nx][ny] > c:
-                    destroyed[nx][ny] = c
-                    queue.append((nx, ny, c))
-                elif not grid[nx][ny] and destroyed[nx][ny] > c + 1:
-                    destroyed[nx][ny] = c + 1
-                    queue.append((nx, ny, c + 1))
+                if maze[nx][ny] == 0:
+                    temp_d += 1
+                if destroyed[nx][ny] > temp_d:
+                    destroyed[nx][ny] = temp_d
+                    queue.append((nx, ny, temp_d))
 
 
-bfs(0, 0, 0)
+
+bfs()
+
 print(destroyed[n - 1][n - 1])

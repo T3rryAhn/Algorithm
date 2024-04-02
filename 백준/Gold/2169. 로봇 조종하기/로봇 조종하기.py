13eecd708e12e1bfb3d02561e27e5a036edd5144
T3rry <t3rryahn.dev@gmail.com>
2024-04-02 23:27:17 +0900

    [Gold II] Title: 로봇 조종하기, Time: 1336 ms, Memory: 94008 KB -BaekjoonHub

--- 백준/Gold/2169. 로봇 조종하기/로봇 조종하기.py
+++ 백준/Gold/2169. 로봇 조종하기/로봇 조종하기.py
@@ -1,30 +1,31 @@
 import sys
 
-n, m = map(int, input().split())
+n, m = map(int, sys.stdin.readline().split())
 
-grid = [list(map(int, input().split())) for _ in range(n)]
+grid = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
 dp = [[0] * m for _ in range(n)]
 
-# 첫 번째 행 초기화
-for x in range(m):
-    dp[0][x] = dp[0][x - 1] + grid[0][x] if x > 0 else grid[0][x]
+# 첫번째행 초기화
+dp[0][0] = grid[0][0]
+for x in range(1, m):
+    dp[0][x] = dp[0][x - 1] + grid[0][x]
 
 for y in range(1, n):
     left = [0] * m
     right = [0] * m
-    
-    # 왼쪽에서 오른쪽으로
+
+    # 왼쪽에서 오른 쪽으로
     left[0] = dp[y - 1][0] + grid[y][0]
     for x in range(1, m):
         left[x] = max(left[x - 1], dp[y - 1][x]) + grid[y][x]
 
-    # 오른쪽에서 왼쪽으로
-    right[m - 1] = dp[y - 1][m - 1] + grid[y][m - 1]
+    # 오른에서 왼
+    right[-1] = dp[y - 1][-1] + grid[y][-1]
     for x in range(m - 2, -1, -1):
         right[x] = max(right[x + 1], dp[y - 1][x]) + grid[y][x]
 
-    # 최댓값 결정
     for x in range(m):
         dp[y][x] = max(left[x], right[x])
 
-print(dp[n - 1][m - 1])
+
+print(dp[-1][-1])

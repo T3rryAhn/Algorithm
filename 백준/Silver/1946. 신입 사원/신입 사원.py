c149375a05a35002add2e944bc409192d8f686cc
T3rry <t3rryahn.dev@gmail.com>
2024-04-03 21:43:18 +0900

    [Silver I] Title: 신입 사원, Time: 2940 ms, Memory: 58536 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.py
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -1,39 +1,22 @@
 import sys
-from collections import deque
 
 
-def newbie_cutter(people: list) -> int:
-    document_rank = [0] * len(people)
-    interview_rank = deque([0] * len(people))   # 면접 등수를 스택으로 구현
-    is_survived = [True] * len(people)          
-
-    for i, person in enumerate(people):
-        document_rank[person[0] - 1] = i
-        interview_rank[person[1] - 1] = i
-
-    for person_num in document_rank:
-        # 탈락했다면 다음 사람으로 넘어가기
-        if not is_survived[person_num]:
-            continue
-        
-        # 서류 등수에서 뽑은사람과 같이 나올때 까지 탈락시키면서 뽑기
-        while interview_rank and person_num != interview_rank[-1]:
-            is_survived[interview_rank[-1]] = False
-            interview_rank.pop()
-        # 서류 등수와 같은사람이 나왔으므로 탈락 안시키면서 뽑기
-        if interview_rank:
-            interview_rank.pop()
-
+def newbie_cutter(people):
+    # 서류 심사 순위를 기준으로 정렬
+    people.sort()
+    max_interview_rank = sys.maxsize
     count = 0
-    for b in is_survived:
-        if b:
-            count += 1
-    return count
 
+    for _, interview_rank in people:
+        if interview_rank < max_interview_rank:
+            count += 1
+            max_interview_rank = interview_rank
+
+    print(count)
 
 t = int(sys.stdin.readline())
 
 for _ in range(t):
     n = int(sys.stdin.readline())
     people = [tuple(map(int, sys.stdin.readline().split())) for _ in range(n)]
-    print(newbie_cutter(people))
+    newbie_cutter(people)

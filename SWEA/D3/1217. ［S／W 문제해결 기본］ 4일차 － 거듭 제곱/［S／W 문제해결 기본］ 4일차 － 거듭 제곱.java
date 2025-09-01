8a3073487d6dd1030d1ac627d3c32490f49858a8
T3rry <t3rryahn.dev@gmail.com>
2025-09-01 17:02:34 +0900

    [D3] Title: [S/W 문제해결 기본] 4일차 - 거듭 제곱, Time: 105 ms, Memory: 26,624 KB -BaekjoonHub

--- SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
+++ SWEA/D3/1217. ［S／W 문제해결 기본］ 4일차 － 거듭 제곱/［S／W 문제해결 기본］ 4일차 － 거듭 제곱.java
@@ -1,22 +1,27 @@
 import java.util.Scanner;
 
 public class Solution {
-	static int power(int a, int b) {
-		if (b <= 0) return 1;
-		
-		return a * power(a, --b);
-	}
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		for (int tc = 1; tc <= 10; tc++) {
-			int caseNum = sc.nextInt();
-			int base = sc.nextInt();
-			int exponent = sc.nextInt();
-			int result = power(base, exponent);
-			
-			System.out.printf("#%d %d%n", caseNum, result);
-		}
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        for (int tc = 1; tc <= 10; tc++) {
+            int caseNum = sc.nextInt();
+
+            System.out.printf("#%d %d%n", caseNum, pow(sc.nextInt(), sc.nextInt()));
+        }
+    }
+
+    static int pow(int c, int n) {
+        if (n == 0)
+            return 1;
+
+        if (n % 2 == 1) {
+            int tmp = pow(c, (n - 1)/2);
+            return tmp * tmp * c;
+        }
+        else {
+            int tmp = pow(c, n / 2);
+            return tmp * tmp;
+        }
+    }
 }

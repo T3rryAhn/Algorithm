2dae634311d73c6bd15bf3137fa7d07a2e81aec0
T3rry <t3rryahn.dev@gmail.com>
2025-09-08 15:27:13 +0900

    [D4] Title: 파핑파핑 지뢰찾기, Time: 571 ms, Memory: 104,968 KB -BaekjoonHub

---
+++ SWEA/D4/1868. 파핑파핑 지뢰찾기/파핑파핑 지뢰찾기.java
@@ -0,0 +1,127 @@
+//package _1868_파핑파핑;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    static int N;
+    static char[][] originTable;
+    static int[][] table;
+    static boolean[][] visited;
+
+    // 8 way left-up, up, right-up, left, right, left-down, down, right-down
+    static int[] dr = { -1, -1, -1, 0, 0, 1, 1, 1 };
+    static int[] dc = { -1, 0, 1, -1, 1, -1, 0, 1 };
+
+    public static void main(String[] args) throws FileNotFoundException {
+        //File file = new File("playground\\_1868_파핑파핑\\input.txt");
+        //Scanner sc = new Scanner(file);
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+        for (int tc = 1; tc <= T; tc++) {
+            // case start
+            N = sc.nextInt();
+            table = new int[N][N];
+            originTable = new char[N][N];
+            visited = new boolean[N][N];
+            Queue<int[]> clickQueue = new ArrayDeque<>();
+
+            // input
+            for (int r = 0; r < N; r++) {
+                String row = sc.next();
+                for (int c = 0; c < N; c++) {
+                    originTable[r][c] = row.charAt(c);
+                }
+            }
+
+            List<int[]> tmpList = new ArrayList<>();
+            // make calculated table
+            for (int r = 0; r < N; r++) {
+                for (int c = 0; c < N; c++) {
+                    char tmp = originTable[r][c];
+
+                    // if bomb
+                    if (tmp == '*') {
+                        visited[r][c] = true;
+                        table[r][c] = -1;
+                        continue;
+                    }
+
+                    // not bomb cell. then mark bombcnt
+                    int bombCnt = 0;
+                    for (int d = 0; d < 8; d++) {
+                        int nr = r + dr[d];
+                        int nc = c + dc[d];
+
+                        if (isInBound(nr, nc)) {
+                            if (originTable[nr][nc] == '*') {
+                                bombCnt++;
+                            }
+                        }
+                    }
+                    table[r][c] = bombCnt;
+                    tmpList.add(new int[] {bombCnt, r, c});
+                }
+            }
+
+            // sort clickQ
+            tmpList.sort((o1, o2) -> o1[0] - o2[0]);
+            clickQueue = new ArrayDeque<>(tmpList);
+            int clickCnt = 0;
+
+            while(!clickQueue.isEmpty()) {
+                int[] curr = clickQueue.poll();
+                // int bombCnt = curr[0];
+                int r = curr[1];
+                int c = curr[2];
+
+                if (visited[r][c]) continue;
+
+                bfs(r, c);
+
+                clickCnt++;
+
+
+            }
+
+            System.out.printf("#%d %d%n", tc, clickCnt);
+
+            // case end
+        }
+        sc.close();
+
+    } // main
+
+    static boolean isInBound(int r, int c) {
+        return r >= 0 && r < N && c >= 0 && c < N;
+    }
+
+    static void bfs(int r, int c) {
+        Queue<int[]> bfsQueue = new ArrayDeque<>();
+
+        bfsQueue.add(new int[] {r, c});
+
+        while(!bfsQueue.isEmpty()) {
+            int[] curr = bfsQueue.poll();
+            int cr = curr[0];
+            int cc = curr[1];
+
+            visited[cr][cc] = true;
+            if(table[cr][cc] != 0) continue;
+            
+            for(int d = 0; d < 8; d++) {
+                int nr = cr + dr[d];
+                int nc = cc + dc[d];
+                
+                if(isInBound(nr, nc) && !visited[nr][nc]) {
+                    visited[nr][nc] = true;
+
+                    if(table[nr][nc] == 0) {
+                        bfsQueue.add(new int[] {nr, nc});
+                    }
+                }
+            }//for
+        }//while
+    }// bfs
+}

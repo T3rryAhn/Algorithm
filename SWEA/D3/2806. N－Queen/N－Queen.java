a5a2eac7a67bbae3719156a3cbb98f198162fe02
T3rry <t3rryahn.dev@gmail.com>
2025-09-14 17:47:02 +0900

    [D3] Title: N-Queen, Time: 104 ms, Memory: 26,496 KB -BaekjoonHub

--- SWEA/D3/2806. N－Queen/N－Queen.java
+++ SWEA/D3/2806. N－Queen/N－Queen.java
@@ -1,76 +1,94 @@
 
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Solution {
-    static int[][] board;
-    static int N;
-    static int count;
+	static int N;
+	static int rowAndColUsedBit;
+	static long diagonalUsedBit;
+	static int count;
+	
+	// 왼위, 위, 오른위
+	static int[] dr = {-1, -1, -1};
+	static int[] dc = {-1, 0, 1};
 
-    static int[] dr = {-1, -1, -1};
-    static int[] dc = {-1, 0, 1};
+	public static void main(String[] args) throws FileNotFoundException {
+//		File file = new File("./src/swea/_2806_nQueen/sample_input (6).txt");
+//		Scanner sc = new Scanner(file);
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		for(int tc = 1; tc <= T; tc++ ) {
+			// case start
+			N = sc.nextInt();
+			rowAndColUsedBit = 0;
+			diagonalUsedBit = 0L;
+			count = 0;
 
-    public static void main(String[] args) throws FileNotFoundException {
-        // File file = new File("playground\\_2806_NQueen\\sample_input (3).txt");
-        // Scanner sc = new Scanner(file);
-        Scanner sc = new Scanner(System.in);
+			dfs(0);
+			
+			System.out.println("#" + tc + " " + count);
+			// case end
+		}
+	}// main
+	
+	static void dfs(int r) {
+		if(r == N) {
+			count++;
+			return;
+		}
+		
+		for(int c = 0; c < N; c++) {
+			if(checkPosByBit(r, c)) {
+				markPosOnBit(r, c);
+				dfs(r + 1);
+				unMarkPosOnBit(r, c);
+			}
+		}
+		
+	}
+	
+	static void unMarkPosOnBit(int r, int c) {
+		// 32 칸중 절반 0 - 15 는 row, 16 - 31 는 col
+		rowAndColUsedBit ^= (1 << r);
+		rowAndColUsedBit ^= ((1 << 16) << c);
+		
+		// 64칸중 0 - 31 왼위 대각선, 32-63 오른위 대각선
+		int dia1Idx = r - c + (N - 1);
+		int dia2Idx = r + c;
+		diagonalUsedBit ^= (1L << dia1Idx);
+		diagonalUsedBit ^= ((1L << 32) << dia2Idx);
+		
+	}
+	
+	
+	static void markPosOnBit(int r, int c) {
+		// 32 칸중 절반 0 - 15 는 row, 16 - 31 는 col
+		rowAndColUsedBit |= (1 << r);
+		rowAndColUsedBit |= ((1 << 16) << c);
+		
+		// 64칸중 0 - 31 왼위 대각선, 32-63 오른위 대각선
+		int dia1Idx = r - c + (N - 1);
+		int dia2Idx = r + c;
+		diagonalUsedBit |= (1L << dia1Idx);
+		diagonalUsedBit |= ((1L << 32) << dia2Idx);
+		
+	}
+	
+	static boolean checkPosByBit(int r, int c) {
+		int queryBit = (1 << r) + ((1 << 16) << c);
+		int dia1Idx = r - c + (N - 1);
+		int dia2Idx = r + c;
+		long queryBit2 = (1L << dia1Idx) + ((1L << 32) << dia2Idx);
+		
+		if((rowAndColUsedBit & queryBit) == 0 && (diagonalUsedBit & queryBit2) == 0) {
+			return true;
+		}
+		
+		return false;
+	}
+	
 
-        int T = sc.nextInt();
-        for(int tc = 1; tc <= T; tc++) {
-            // case start
-            N = sc.nextInt();
-            count = 0;
-
-            board = new int[N][N];
-            setQueen(0);
-
-            System.out.printf("#%d %d%n", tc, count);
-            // case end
-        }
-
-    }
-
-    static void setQueen(int row) {
-        if(row == N){
-            count++;
-            return;
-        }
-
-        
-        for(int c = 0; c < N; c++) {
-            if(isValid(row, c)) {
-                board[row][c] = 1;
-                setQueen(row + 1);
-                board[row][c] = 0;
-            }
-        }
-    }
-
-    static boolean isValid(int row, int col) {
-        boolean result = true;
-
-        for(int d = 0; d < 3; d++) {
-            if(!result) break;
-
-            int nextR = row + dr[d];
-            int nextC = col + dc[d];
-
-            while(isInBound(nextR, nextC)) {
-                if(board[nextR][nextC] == 0) {
-                    nextR += dr[d];
-                    nextC += dc[d];
-                } else {
-                    result = false;
-                    break;
-                }
-            }
-        }
-
-        return result;
-    }
-
-    static boolean isInBound(int row, int col) {
-        return row >= 0 && row < N && col >= 0 && col < N;
-    }
 }

ff49a3f93a0d7c0bda79d13d9d6a6f666b282dad
T3rry <t3rryahn.dev@gmail.com>
2026-01-25 12:04:24 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 88.21 ms, Memory: 98.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,28 @@
+// hashmap
+// 참가자 명단을 hashmap에 등록 (참가자이름 : 인원)
+// 완주자 명단을 보며 장부에서 숫자 빼기
+// 최종 확인시 숫자가 남은 이름이 미완주자
+
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        Map<String, Integer> map = new HashMap<>();
+        
+        for (String p : participant) {
+            map.put(p, map.getOrDefault(p, 0) + 1);
+        }
+        
+        for (String c : completion) {
+            map.put(c, map.get(c) - 1);
+        }
+        
+        for (String key : map.keySet()) {
+            if (map.get(key) != 0) {
+                return key;
+            }
+        }
+        
+        return "";
+    }
+}
